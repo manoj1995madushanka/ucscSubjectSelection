fae623e8f57949e498698e867e512f968bad4bf4
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-05-04 08:45:53 +0530

    config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    /*apiKey: 'AIzaSyAtm1R9ORNe3gmTz_YAELhrr63PXtQRTQE',
+    authDomain: 'nannylk-83988.firebaseapp.com',
+    databaseURL: 'https://nannylk-83988.firebaseio.com',
+    projectId: 'nannylk-83988',
+    storageBucket: 'nannylk-83988.appspot.com',
+    messagingSenderId: '120740652673'*/
+    apiKey: "AIzaSyCohcCLL2hNaMZO0fR-jBKSPEMek9ykVQM",
+    authDomain: "subjectselectionucsc.firebaseapp.com",
+    databaseURL: "https://subjectselectionucsc.firebaseio.com",
+    projectId: "subjectselectionucsc",
+    storageBucket: "subjectselectionucsc.appspot.com",
+    messagingSenderId: "614016979434",
+    appId: "1:614016979434:web:fb0fbae8b3e8a38b"
+  }
 };
 
 /*
