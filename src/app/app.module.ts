0e441a64543738a692b02689799fd87851d0bfc0
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-05-04 09:21:21 +0530

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
 import { AuthComponent } from './auth/auth.component';
+import {RouterModule} from '@angular/router';
+import {RoutingModule} from './routing.module';
+import {environment} from '../environments/environment';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireStorageModule} from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -12,7 +21,16 @@ import { AuthComponent } from './auth/auth.component';
     AuthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatButtonModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
