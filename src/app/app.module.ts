cedb0266547c2a69ad48e0e4c66782ea718b0f21
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-05-04 08:36:39 +0530

    folder structure for students

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule
