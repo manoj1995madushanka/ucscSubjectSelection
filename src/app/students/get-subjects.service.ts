f6a6d07a1deeff9250deb5dcef14120e2f46cee4
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-05-25 10:31:39 +0530

    datasource change method

--- src/app/students/get-subjects.service.ts
+++ src/app/students/get-subjects.service.ts
@@ -4,6 +4,7 @@ import {SubjectsModel} from '../models/Subjects.model';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {Router} from '@angular/router';
+import {and} from '@angular/router/src/utils/collection';
 
 @Injectable({
   providedIn: 'root'
@@ -52,7 +53,75 @@ export class GetSubjectsService {
       subscribe((subs: SubjectsModel[]) => {
         this.subjectChanged.next(subs);
       });
+      this.resetValues();
     }
+    if (this.cs4cs === true && this.cs3yr1sem === true) {
+      this.db.collection('bsc_cs_3yr1sem').valueChanges().
+      subscribe((subs: SubjectsModel[]) => {
+        this.subjectChanged.next(subs);
+      });
+      this.resetValues();
+    }
+
+    if (this.cs4cs === true && this.cs4yr1sem === true) {
+      this.db.collection('bsc_cs_4yr1sem').valueChanges().
+      subscribe((subs: SubjectsModel[]) => {
+        this.subjectChanged.next(subs);
+      });
+      this.resetValues();
+    }
+
+    if (this.cs4cs === true && this.cs4yr2sem === true) {
+      this.db.collection('bsc_cs_4yr2sem').valueChanges().
+      subscribe((subs: SubjectsModel[]) => {
+        this.subjectChanged.next(subs);
+      });
+      this.resetValues();
+    }
+
+    if (this.cs4se === true ) {
+      this.db.collection('bsc_se').valueChanges().
+      subscribe((subs: SubjectsModel[]) => {
+        this.subjectChanged.next(subs);
+      });
+      this.resetValues();
+    }
+
+    if (this.cs4se === true && this.se3yr1sem === true) {
+      this.db.collection('bsc_se_3yr1sem').valueChanges().
+      subscribe((subs: SubjectsModel[]) => {
+        this.subjectChanged.next(subs);
+      });
+      this.resetValues();
+    }
+
+    if (this.cs4se === true && this.se4yr1sem === true) {
+      this.db.collection('bsc_se_4yr1sem').valueChanges().
+      subscribe((subs: SubjectsModel[]) => {
+        this.subjectChanged.next(subs);
+      });
+      this.resetValues();
+    }
+
+    if (this.cs4se === true && this.se4yr2sem === true) {
+      this.db.collection('bsc_se_4yr2sem').valueChanges().
+      subscribe((subs: SubjectsModel[]) => {
+        this.subjectChanged.next(subs);
+      });
+      this.resetValues();
+    }
+  }
+
+  resetValues() {
+    this.csGeneral = false;
+    this.cs4se = false;
+    this.se3yr1sem = false;
+    this.se4yr1sem = false;
+    this.se4yr2sem = false;
+    this.cs4cs = false;
+    this.cs3yr1sem = false;
+    this.cs4yr1sem = false;
+    this.cs4yr2sem = false;
   }
 
 }
