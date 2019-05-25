471aff13d6bff938d99cd5dcbabdb528cc7171db
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-05-25 09:27:19 +0530

    se component

--- src/app/students/se-select-year-sem/se-select-year-sem.component.ts
+++ src/app/students/se-select-year-sem/se-select-year-sem.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-se-select-year-sem',
   templateUrl: './se-select-year-sem.component.html',
-  styleUrls: ['./se-select-year-sem.component.sass']
+  styleUrls: ['./se-select-year-sem.component.css']
 })
 export class SeSelectYearSemComponent implements OnInit {
 
